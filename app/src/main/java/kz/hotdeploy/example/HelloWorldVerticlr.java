2dc84360e30d5240ca747ac10049ffa5427f4899
Галихан Хамитов <galikhan.khamitov@zee.kz>
2021-01-26 22:42:41 +0600

    Init commit

---
+++ app/src/main/java/kz/hotdeploy/example/HelloWorldVerticlr.java
@@ -0,0 +1,28 @@
+package kz.hotdeploy.example;
+
+
+        import io.vertx.core.AbstractVerticle;
+        import io.vertx.core.Future;
+        import io.vertx.core.Promise;
+        import io.vertx.core.Vertx;
+        import io.vertx.core.http.HttpServer;
+
+/**
+ * @author <a href="http://alexvetter.de">Alexander Vetter</a>
+ */
+public class HelloWorldVerticlr extends AbstractVerticle {
+
+    private HttpServer httpServer;
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+        System.out.println("Всеравно Now started WORKING");
+        System.out.println("Всеравно Now started WORKING");
+        System.out.println("Всеравно Now started WORKING");
+        System.out.println("Всеравно Now started WORKING");
+        System.out.println("Всеравно Now started WORKING");
+    }
+
+}
+
